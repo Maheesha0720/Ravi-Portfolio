04dafa421e78f6a74be3773c2814b8cb755e964f
Maheesha0720 <maheeshamaduwanthi2002@gmail.com>
2025-11-19 12:49:17 +0530

    new changes

---
+++ projectFiles.js
@@ -0,0 +1,66 @@
+const projectData = {
+  "3D DESIGN": [
+    { img: "../asset/img/Bid Nest-01.png", desc: "3D branding concept" },
+    { img: "../asset/img/iron paradise.jpg", desc: "3D gym logo" },
+    {
+      img: "../asset/img/Media T-Shirt Design Final PSD.png",
+      desc: "3D apparel mockup",
+    },
+  ],
+  "LOGO DESIGN": [
+    { img: "../asset/img/Bid Nest-01.png", desc: "3D branding concept" },
+    { img: "../asset/img/iron paradise.jpg", desc: "3D gym logo" },
+    {
+      img: "../asset/img/Media T-Shirt Design Final PSD.png",
+      desc: "3D apparel mockup",
+    },
+  ],
+  "T-SHIRT DESIGN": [
+    { img: "../asset/img/Bid Nest-01.png", desc: "3D branding concept" },
+    { img: "../asset/img/iron paradise.jpg", desc: "3D gym logo" },
+    {
+      img: "../asset/img/Media T-Shirt Design Final PSD.png",
+      desc: "3D apparel mockup",
+    },
+  ],
+  "SOCIAL MEDIA": [
+    { img: "../asset/img/Bid Nest-01.png", desc: "3D branding concept" },
+    { img: "../asset/img/iron paradise.jpg", desc: "3D gym logo" },
+    {
+      img: "../asset/img/Media T-Shirt Design Final PSD.png",
+      desc: "3D apparel mockup",
+    },
+  ],
+  POSTERS: [
+    { img: "../asset/img/Bid Nest-01.png", desc: "3D branding concept" },
+    { img: "../asset/img/iron paradise.jpg", desc: "3D gym logo" },
+    {
+      img: "../asset/img/Media T-Shirt Design Final PSD.png",
+      desc: "3D apparel mockup",
+    },
+  ],
+  " COVER PAGES": [
+    { img: "../asset/img/Bid Nest-01.png", desc: "3D branding concept" },
+    { img: "../asset/img/iron paradise.jpg", desc: "3D gym logo" },
+    {
+      img: "../asset/img/Media T-Shirt Design Final PSD.png",
+      desc: "3D apparel mockup",
+    },
+  ],
+  "BRAND IDENTITY": [
+    { img: "../asset/img/Bid Nest-01.png", desc: "3D branding concept" },
+    { img: "../asset/img/iron paradise.jpg", desc: "3D gym logo" },
+    {
+      img: "../asset/img/Media T-Shirt Design Final PSD.png",
+      desc: "3D apparel mockup",
+    },
+  ],
+  "OTHER DESIGNS": [
+    { img: "../asset/img/Bid Nest-01.png", desc: "3D branding concept" },
+    { img: "../asset/img/iron paradise.jpg", desc: "3D gym logo" },
+    {
+      img: "../asset/img/Media T-Shirt Design Final PSD.png",
+      desc: "3D apparel mockup",
+    },
+  ],
+};
